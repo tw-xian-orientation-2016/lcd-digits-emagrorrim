865835a06427299665f60a6b8f0a6598de899baa
emagrorrom <373407641@qq.com>
2016-03-02 16:15:12 +0800

    finish splitInLine

--- lcd-digits/main/main.js
+++ lcd-digits/main/main.js
@@ -10,3 +10,15 @@ function convertToLCD(digits, lcdNumbers) {
   });
   return lcdShownNumbers;
 }
+
+function splitInLine(lcdShownNumbers) {
+  var firstLineStrings = [];
+  var secondLineStrings = [];
+  var thirdLineStrings = []
+  lcdShownNumbers.forEach(function(lcdShownNumber) {
+     firstLineStrings.push(lcdShownNumber.substr(0, 3));
+     secondLineStrings.push(lcdShownNumber.substr(3, 3));
+     thirdLineStrings.push(lcdShownNumber.substr(6, 3));
+  });
+  return [firstLineStrings, secondLineStrings, thirdLineStrings];
+}
