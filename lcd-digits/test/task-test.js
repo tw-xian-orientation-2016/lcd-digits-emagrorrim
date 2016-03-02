49fca76d3194fd40ad0512898ff6387150e43226
emagrorrom <373407641@qq.com>
2016-03-02 16:17:05 +0800

    finish join test

--- lcd-digits/test/task-test.js
+++ lcd-digits/test/task-test.js
@@ -54,3 +54,28 @@ describe('splitInLine()', function() {
     expect(output).toEqual(expectResult);
   });
 });
+
+describe('join()', function() {
+  var input;
+
+  beforeEach(function() {
+    input = [
+      ['._.', '...', '._.'],
+      ['|_|', '..|', '|.|'],
+      ['..|', '..|', '|_|']
+    ];
+  });
+
+  it('should join lcd number in line', function() {
+
+    var output = splitInLine(input);
+
+    var expectResult = [
+      ['._. ... ._.'],
+      ['|_| ..| |.|'],
+      ['..| ..| |_|']
+    ];
+
+    expect(output).toEqual(expectResult);
+  });
+});
